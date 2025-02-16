a2095ef3953e9565a946fc5230a5f888890c7677
QuantBender <ameck.dosseh@um6p.ma>
2025-02-16 08:53:04 +0100

    initial

--- model.py
+++ model.py
@@ -8,6 +8,34 @@
 # <!> *SAVE YOUR PARAMETERS IN THE parameters/ DICRECTORY* <!>
 ############################################################################
 
+import whisper
+from TTS.api import TTS
+from IPython.display import Audio
+import torch
+import numpy as np
+import random
+
+# Set random seed for reproducibility
+def set_seed(seed):
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+# Set the seed
+set_seed(0x0BADC0DE)
+
+device = torch.cuda.current_device() if torch.cuda.is_available() else 'cpu'
+speech_to_test_model = whisper.load_model("large-v2") # large-v2, medium
+test_to_speech_model = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+
+speaker_list = test_to_speech_model.speakers
+
+
 def anonymize(input_audio_path): # <!> DO NOT ADD ANY OTHER ARGUMENTS <!>
     """
     anonymization algorithm
@@ -25,13 +53,28 @@ def anonymize(input_audio_path): # <!> DO NOT ADD ANY OTHER ARGUMENTS <!>
     sr : int
         The sample rate of the processed audio.
     """
+    speaker = str(input_audio_path).split('/')[2]
+    speaker_id = int(speaker.replace('speaker', '')) - 1
+    # print(f"<<<<<<<<<<<<<<<<<<<<<<<<<{speaker_id}>>>>>>>>>>>>>>>>>>>>>")
 
     # Read the source audio file
+    #audio = Audio("/content/1272-128104-0000.wav")
 
     # Apply your anonymization algorithm
-    
+    # 01
+    result = speech_to_test_model.transcribe(input_audio_path, language="en")
+    transcribe = result["text"]
+    # if transcribe.count(".") > 1:
+    #     transcribe = ', '.join(transcribe.rsplit('.', maxsplit=1))  
+    # transcribe = result["text"].replace('.', ', ')
+    # transcribe = transcribe.replace('!', '  ')
+    # transcribe = transcribe.replace('?', '?, ')
+    # 02
+    # test_to_speech_model.tts_to_file(transcribe, file_path="out.wav")
+    audio_array = test_to_speech_model.tts(transcribe, language="en", speaker=speaker_list[speaker_id])
+
     # Output:
-    audio = ...
-    sr = ...
+    audio = audio_array
+    sr = 22050
     
     return audio, sr
--- run.sh
+++ run.sh
@@ -11,28 +11,28 @@ run() {
     echo "Running evaluation ..."
 
     # Determine the correct Python command
-    PYTHON_CMD=$(command -v python3.12 || command -v python || command -v python3)
-    if [ -z "$PYTHON_CMD" ]; then
-        echo "Python not found! Please install Python 3.12"
-        exit 1
-    fi
+    # PYTHON_CMD=$(command -v python3.12 || command -v python || command -v python3)
+    # if [ -z "$PYTHON_CMD" ]; then
+    #     echo "Python not found! Please install Python 3.12"
+    #     exit 1
+    # fi
 
-    # Check if virtual environment exists
-    if [ ! -d ".venv" ]; then
-        echo "Virtual environment not found! Creating one..."
-        $PYTHON_CMD -m venv .venv
-    fi
+    # # Check if virtual environment exists
+    # if [ ! -d ".venv" ]; then
+    #     echo "Virtual environment not found! Creating one..."
+    #     $PYTHON_CMD -m venv .venv
+    # fi
 
-    # Activate virtual environment (different paths for Windows and Unix)
-    if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
-        source .venv/Scripts/activate
-    else
-        source .venv/bin/activate
-    fi
+    # # Activate virtual environment (different paths for Windows and Unix)
+    # if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+    #     source .venv/Scripts/activate
+    # else
+    #     source .venv/bin/activate
+    # fi
 
-    # Ensure required packages are installed
-    python -m pip install --upgrade pip
-    pip install -r requirements.txt -q
+    # # Ensure required packages are installed
+    # python -m pip install --upgrade pip
+    # pip install -r requirements.txt -q
 
     # Set evaluation directory (default: "evaluation_data/")
     EVAL_DIR=${1:-"evaluation_data/"}
